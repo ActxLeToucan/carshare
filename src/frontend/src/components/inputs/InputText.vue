4e04c04f7f047834f7c7b7a07af06908cb658605
FurWaz <fur.waz06@gmail.com>
2023-02-17 19:13:52 +0100

    Created text input

---
+++ src/frontend/src/components/inputs/InputText.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="flex grow h-fit w-full md:flex-row flex-col justify-between md:space-x-8 md:items-center my-2">
+        <label class="flex text-xl text-slate-500 font-bold whitespace-nowrap text-ellipsis w-fit">
+            {{ label }}
+        </label>
+        <input
+            class="flex h-fit bg-white border-b-4 border-slate-200 rounded-md px-4 py-2 text-gray-700 font-bold text-lg hover:border-slate-300 focus:border-teal-500 transition-all focus:outline outline-transparent"
+            :placeholder="placeholder"
+            :type="type"
+            :value="value"
+        >
+    </div>
+</template>
+
+<script>
+export default {
+    name: '',
+    props: {
+        label: {
+            type: String,
+            default: '',
+            required: false
+        },
+        placeholder: {
+            type: String,
+            default: '',
+            required: false
+        },
+        value: {
+            type: String,
+            default: '',
+            required: false
+        },
+        type: {
+            type: String,
+            default: 'text',
+            required: false
+        }
+    },
+    data() {
+        return {}
+    }
+}
+</script>
