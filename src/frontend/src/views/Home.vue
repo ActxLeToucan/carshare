ab2da40125d54b7a3786984965b8d021de00ba95
FurWaz <fur.waz06@gmail.com>
2023-02-17 18:39:14 +0100

    Added topbar to main page

--- src/frontend/src/views/Home.vue
+++ src/frontend/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
-    <div class="flex grow flex-col justify-center px-4">
-        <div class="w-fit space-y-6 mx-auto">
+    <div class="flex grow flex-col">
+        <topbar></topbar>
+        <div class="flex grow w-fit flex-col justify-center space-y-6 mx-auto">
             <h1 class="md:text-6xl text-4xl font-extrabold text-teal-500 text-center"> Car Share - Application </h1>
             <img src="../assets/img/car.svg" class="max-w-full md:max-h-20 max-h-12 mx-auto" alt="">
         </div>
@@ -8,7 +9,10 @@
 </template>
 
 <script>
+import Topbar from "../components/topbar/Topbar.vue"
+
 export default {
+    components: { Topbar },
     name: 'Home',
     data() {
         return {}
