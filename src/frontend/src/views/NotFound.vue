da68b0f51ce640d2375e373fba1f29f84dda50b6
FurWaz <fur.waz06@gmail.com>
2023-02-19 18:36:44 +0100

    Set error message to french

--- src/frontend/src/views/NotFound.vue
+++ src/frontend/src/views/NotFound.vue
@@ -4,7 +4,7 @@
         <div class="flex grow flex-col justify-center w-fit space-y-6 mx-auto">
             <h1 class="md:text-6xl text-4xl font-extrabold text-teal-500 text-center"> Car Share - Application </h1>
             <img src="../assets/img/car.svg" class="max-w-full md:max-h-20 max-h-12 mx-auto" alt="">
-            <p class="md:text-4xl text-xl font-bold text-slate-400 text-center"> Woops, the page you are looking for does not exist. </p>
+            <p class="md:text-4xl text-xl font-bold text-slate-400 text-center"> Woops, la page que vous recherchez n'existe pas ! </p>
         </div>
     </div>
 </template>
