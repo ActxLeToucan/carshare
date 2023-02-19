e08fa935a28124f58ceec6081bca49654a28627d
FurWaz <fur.waz06@gmail.com>
2023-02-19 23:52:27 +0100

    Added user input verification

--- src/frontend/src/views/Register.vue
+++ src/frontend/src/views/Register.vue
@@ -23,12 +23,54 @@ import InputSwitch from "../components/inputs/InputSwitch.vue";
 import { Log } from '../scripts/Logs';
 import User from '../scripts/User';
 
+function isPhoneNumber(val) {
+    if (!val) return false;
+    return val.replace(/(\.|\s|-)/g, "").trim().match(/^((00[0-9]{2})?0[0-9][0-9]{8}|\+[0-9]{11,12})$/) != null;
+}
+
+const field_checks = [
+    {field: "firstname",        check: (value) => value.length > 0, error: "Veuillez renseignez votre nom."},
+    {field: "lastname",         check: (value) => value.length > 0, error: "Veuillez renseignez votre prénom."},
+    {field: "mail",             check: (value) => value.length > 0, error: "Veuillez renseignez votre adresse mail."},
+    {field: "phone",            check: (value) => value.length > 0, error: "Veuillez renseignez votre numéro de téléphone."},
+    {field: "password",         check: (value) => value.length > 0, error: "Veuillez renseignez votre mot de passe."},
+    {field: "password-confirm", check: (value) => value.length > 0, error: "Veuillez confirmer votre mot de passe."},
+
+    {field: "firstname",        check: (value) => value.length <= 50,   error: "Ce nom est trop long."},
+    {field: "lastname",         check: (value) => value.length <= 50,   error: "Ce prénom est trop long."},
+    {field: "mail",             check: (value) => value.length <= 64,   error: "Cette adresse mail est trop longue."},
+    {field: "phone",            check: (value) => isPhoneNumber(value), error: "Ce numéro de téléphone est invalide."},
+
+    {field: "password-confirm", check: (value, modal) => value === modal.get("password"), error: "Les mots de passe ne correspondent pas."},
+    {field: "password",         check: (value) => value.length >= 10,                     error: "Votre mot de passe doit faire au moins 10 caractères."},
+    {field: "password",         check: (value) => value.match(/[A-Z]/g) != null,          error: "Votre mot de passe doit contenir au moins une majuscule."},
+    {field: "password",         check: (value) => value.match(/[a-z]/g) != null,          error: "Votre mot de passe doit contenir au moins une minuscule."},
+    {field: "password",         check: (value) => value.match(/[0-9]/g) != null,          error: "Votre mot de passe doit contenir au moins un chiffre."},
+    {field: "password",         check: (value) => value.match(/[^A-Za-z0-9]/g) != null,   error: "Votre mot de passe doit contenir au moins un caractère spécial."}
+];
+
 function onCancel(modal) {
     return true;
 }
 
 function onValidate(modal) {
     return new Promise((resolve, reject) => {
+        
+        const inputs_log = modal.log("Vérification des entrées ...", Log.INFO);
+        for (let i = 0; i < field_checks.length; i++) {
+            const check = field_checks[i];
+            const result = check.check(modal.get(check.field), modal);
+            if (!result) {
+                modal.focus(check.field);
+                inputs_log.update(check.error, Log.ERROR);
+                setTimeout(() => { inputs_log.delete(); }, 2000);
+                resolve(false);
+                return;
+            }
+        }
+        inputs_log.update("Vérification des entrées ... OK", Log.SUCCESS);
+        setTimeout(() => { inputs_log.delete() }, 500);
+
         const err_log = modal.log("Erreur : Inscription non implémentée.", Log.ERROR);
         setTimeout(() => {
             err_log.delete();
