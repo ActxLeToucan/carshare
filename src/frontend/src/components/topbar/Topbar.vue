f8a57dd4829820ed1ffbf9e7e8ade2d58dc56caa
FurWaz <fur.waz06@gmail.com>
2023-02-19 18:43:53 +0100

    Minor topbar changes
    Replaced login/register buttons by profile
    Added app name animation on hover

--- src/frontend/src/components/topbar/Topbar.vue
+++ src/frontend/src/components/topbar/Topbar.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="flex grow-0 h-fit w-full px-4 py-2 bg-slate-50 border-b-8 border-teal-500 items-center">
         <router-link to="/" class="flex w-[20%] h-fit">
-            <h1 class="text-slate-500 text-3xl font-extrabold"> Car Share </h1>
+            <h1 class="text-slate-500 text-3xl font-extrabold hover:text-teal-500 transition-all"> Car Share </h1>
         </router-link>
         <div class="flex grow justify-evenly">
             <button-text v-for="button in buttons" :key="button.name" :href="button.link">
@@ -9,8 +9,7 @@
             </button-text>
         </div>
         <div class="flex w-[20%] h-fit justify-end space-x-4">
-            <button-text href="/register"> S'inscrire </button-text>
-            <button-block href="/login"> Se connecter </button-block>
+            <button-block href="/profile"> Mon profil </button-block>
         </div>
     </div>
 </template>
