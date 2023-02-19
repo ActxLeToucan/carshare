92119a5825111ec2dba80091d52ac103f9918372
Antoine CONTOUX <contoux1u@etu.univ-lorraine.fr>
2023-02-20 00:15:57 +0100

    [api] color type log

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -4,31 +4,35 @@ import path from 'path';
 import util from 'util';
 import { app } from './app';
 
-if (process.env.NODE_ENV !== 'development') {
+function setupLogging () {
     const logFile = fs.createWriteStream(path.join(__dirname, '../console.log'), { flags: 'a' });
     const logFileError = fs.createWriteStream(path.join(__dirname, '../console.error.log'), { flags: 'a' });
     const logStdout = process.stdout;
 
-    function consoleToFile (d: any, type: string = 'info') {
+    function consoleToFile (d: any, type: string = 'info', color: string = '\x1b[0m') {
         const now = new Date();
-        logFile.write(`${now.toISOString()}\t[${type}]\t${util.format(d)}\n`);
-        logStdout.write(util.format(d) + '\n');
+        if (process.env.NODE_ENV !== 'development')logFile.write(`${now.toISOString()}\t[${type}]\t${util.format(d)}\n`);
+        logStdout.write(`${color}[${type}]\x1b[0m\t${util.format(d)}\n`);
     }
 
-    console.log = (d) => { consoleToFile(d, 'info'); };
-    console.info = (d) => { consoleToFile(d, 'info'); };
-    console.warn = (d) => { consoleToFile(d, 'warn'); };
-    console.debug = (d) => { consoleToFile(d, 'debug'); };
-    console.trace = (d) => { consoleToFile(d, 'trace'); };
+    console.log = (d) => { consoleToFile(d); };
+    console.info = (d) => { consoleToFile(d); };
+    console.warn = (d) => { consoleToFile(d, 'warn', '\x1b[33m'); };
+    console.debug = (d) => { consoleToFile(d, 'debug', '\x1b[90m'); };
 
     console.error = function (d) {
         const now = new Date();
-        logFileError.write(`${now.toISOString()}\n\n${util.format(d)}\n\n====================\n\n`);
-        logStdout.write(util.format(d) + '\n');
+        if (process.env.NODE_ENV !== 'development') logFileError.write(`${now.toISOString()}\n\n${util.format(d)}\n\n====================\n\n`);
+        logStdout.write(`\x1b[31m[error]\x1b[0m\t${util.format(d)}\n`);
     };
 }
+setupLogging();
 
-console.log(`Environment: ${process.env.NODE_ENV ?? 'undefined'}`);
+if (process.env.NODE_ENV === undefined || process.env.NODE_ENV === '') {
+    console.warn('NODE_ENV environment variable is not set.');
+} else {
+    console.log('Environment: ' + process.env.NODE_ENV);
+}
 
 if (process.env.JWT_SECRET === undefined || process.env.JWT_SECRET === '' || process.env.JWT_SECRET === '<change_secret>') {
     console.error('JWT_SECRET environment variable is not set.');
