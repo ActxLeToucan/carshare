f64e3dc1d4e19847171af52fb2b5eff2a9516e5e
FurWaz <fur.waz06@gmail.com>
2023-02-23 01:03:33 +0100

    Removed miss-committed file

--- src/frontend/src/config.js
+++
@@ -1,9 +0,0 @@
-/**********************************************
-     Front-end API class configuration file
- **********************************************/
-
-export default {
-    api: {
-        url: 'https://carshare.apis.furwaz.fr'
-    }
-};
