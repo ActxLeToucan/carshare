02bc7a664cd3a945d6dc92af96cfc168e3fa91fe
FurWaz <fur.waz06@gmail.com>
2023-02-23 16:33:27 +0100

    Fixed url reset at button press

--- src/frontend/src/components/inputs/ButtonBlock.vue
+++ src/frontend/src/components/inputs/ButtonBlock.vue
@@ -1,11 +1,19 @@
 <template>
-    <router-link
-        ref="btn"
-        :to="href ?? ''"
-        class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-white rounded-md bg-slate-100 border-b-4 border-slate-200
-               hover:bg-teal-500 hover:text-slate-50 hover:shadow-md hover:border-teal-600 transition-all">
-        <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
-    </router-link>
+    <div>
+        <router-link v-if="href"
+            ref="btn"
+            :to="href ?? ''"
+            class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-white rounded-md bg-slate-100 border-b-4 border-slate-200
+                hover:bg-teal-500 hover:text-slate-50 hover:shadow-md hover:border-teal-600 transition-all">
+            <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
+        </router-link>
+        <button v-if="!href"
+            ref="btn-2"
+            class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-white rounded-md bg-slate-100 border-b-4 border-slate-200
+                hover:bg-teal-500 hover:text-slate-50 hover:shadow-md hover:border-teal-600 transition-all">
+            <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
+        </button>
+    </div>
 </template>
 
 <script>
@@ -35,7 +43,7 @@ export default {
         }
     },
     mounted() {
-        const el = this.$refs["btn"].$el;
+        const el = this.href ? this.$refs["btn"].$el : this.$refs["btn-2"];
         if (this.disabled) {
             el.classList.remove("text-slate-500", "hover:bg-teal-500", "hover:text-slate-50", "hover:shadow-md", "hover:border-teal-600");
             el.classList.add("text-slate-400", "cursor-default");
--- src/frontend/src/components/inputs/ButtonText.vue
+++ src/frontend/src/components/inputs/ButtonText.vue
@@ -1,11 +1,19 @@
 <template>
-    <router-link
-        ref="btn"
-        :to="href ?? ''"
-        class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-slate-50 rounded-md
-               border-b-4 border-transparent hover:bg-slate-100 hover:border-slate-200 hover:text-teal-500 transition-all">
-        <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
-    </router-link>
+    <div>
+        <router-link v-show="href"
+            ref="btn"
+            :to="href ?? ''"
+            class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-slate-50 rounded-md
+                border-b-4 border-transparent hover:bg-slate-100 hover:border-slate-200 hover:text-teal-500 transition-all">
+            <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
+        </router-link>
+        <button v-show="!href"
+            ref="btn-2"
+            class="flex items-center justify-center w-fit h-fit py-2 px-4 text-slate-500 text-xl font-bold bg-slate-50 rounded-md
+                border-b-4 border-transparent hover:bg-slate-100 hover:border-slate-200 hover:text-teal-500 transition-all">
+            <p class="whitespace-nowrap text-ellipsis max-w-full min-w-0 w-fit h-fit max-h-full min-h-0"> <slot></slot> </p>
+        </button>
+    </div>
 </template>
 
 <script>
@@ -35,7 +43,7 @@ export default {
         }
     },
     mounted() {
-        const el = this.$refs["btn"].$el;
+        const el = this.href ? this.$refs["btn"].$el : this.$refs["btn-2"];
         if (this.disabled) {
             el.classList.remove("text-slate-500", "hover:bg-slate-100", "hover:text-teal-500", "hover:border-slate-200");
             el.classList.add("text-slate-400", "cursor-default");
