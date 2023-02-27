da2ecbc776c9c676104199ce1ce9cba445cb3c10
dranerine <guillaumere2002@gmail.com>
2023-02-27 18:43:32 +0100

    [api] GET /notifications/my
    Get all the notifications of the current user

---
+++ src/backend/src/controllers/notifications.ts
@@ -0,0 +1,23 @@
+import type express from 'express';
+import { prisma } from '../app';
+import { error, sendMsg } from '../tools/translator';
+
+exports.myNotifications = (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    if (res.locals.user === undefined) {
+        sendMsg(req, res, error.auth.noToken);
+        return;
+    }
+
+    prisma.user.findMany({
+        where: { id: res.locals.user.id },
+        select: {
+            notifications: true
+        }
+
+    }).then(notifications => {
+        res.status(200).json(notifications);
+    }).catch((err) => {
+        console.error(err);
+        sendMsg(req, res, error.generic.internalError);
+    });
+}
