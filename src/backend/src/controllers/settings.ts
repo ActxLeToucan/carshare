5e5613e552bd563e864aa4d19e77db9628bb6321
RemiDNancy <remi.dero7@etu.univ-lorraine.fr>
2023-02-28 21:43:11 +0100

    corrected doc + runned lint

--- src/backend/src/controllers/settings.ts
+++ src/backend/src/controllers/settings.ts
@@ -9,7 +9,7 @@ exports.getSettingsNotifications = (req: express.Request, res: express.Response,
         return;
     }
 
-    res.status(200).json({value: res.locals.user.mailNotif});
+    res.status(200).json({ value: res.locals.user.mailNotif });
 }
 
 exports.updateSettingsNotifications = (req: express.Request, res: express.Response, next: express.NextFunction) => {
@@ -18,16 +18,16 @@ exports.updateSettingsNotifications = (req: express.Request, res: express.Respon
         return;
     }
 
-    const mailNotif = req.body.value;
+    const value = req.body.value;
 
-    if (mailNotif !== undefined && !properties.checkBooleanField(mailNotif, req, res, 'mailNotif')) {
+    if (value !== undefined && !properties.checkBooleanField(value, req, res, 'mailNotif')) {
         sendMsg(req, res, error.boolean.type);
         return;
     }
 
     prisma.user.update({
         where: { id: res.locals.user.id },
-        data: { mailNotif: mailNotif }
+        data: { mailNotif: value }
     }).then(() => {
         sendMsg(req, res, info.settings.saved);
     }).catch((err) => {
