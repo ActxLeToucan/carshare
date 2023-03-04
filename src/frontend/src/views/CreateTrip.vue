45ab477e5a9f1edf26a9aa22c448aff653d34c9d
kenza <kenza.bouhadoun9@univ-lorraine.fr>
2023-03-05 00:31:17 +0100

    ajouter la creation du groupe

---
+++ src/frontend/src/views/CreateTrip.vue
@@ -0,0 +1,80 @@
+<template>
+    <div class="flex grow flex-col">
+        <topbar v-if="User.CurrentUser != null"></topbar>
+        <button-block class="show-right flex flex-col items-center md:w-min w-full  mx-auto overflow-hidden text-xl" style="margin-top: 1em"> Nouveau trajet </button-block>
+        <p class="show-down text-xl text-xl text-slate-400 text-left font-bold position: left  ml-4"> Type de trajet </p>
+        <div class="flex items-center">
+            <p class="text-xl text-slate-400 font-bold  ml-4 "> Nombre de places </p>
+            <button class="bg-gray-300  text-white font-bold py-2 px-4 rounded ml-4" @click="incrementCounter">{{ compteur }}</button>
+        </div>
+
+
+        <div>
+            <div class="flex items-center">
+                <p class="show-down text-xl text-xl text-slate-400 text-left font-bold position: left  ml-4"> Départ </p>
+                <selector ref="startSelector" :oncompletion="searchCities" :onclick="onstartselected" x="1" y="4"></selector>
+                <input-text name="startpoint" class="w-45 mx-auto max-w-fit" placeholder="Nancy" dark="true"></input-text>
+            </div>
+       <!-- <button-block class="w-fit" color="slate" :action="reverseInputs"> 
+            <arrows-right-left-icon class="md:block hidden h-7 w-7"></arrows-right-left-icon>
+            <arrows-up-down-icon class="md:hidden block h-7 w-7"></arrows-up-down-icon>
+        </button-block> -->
+            <div class="flex items-center">
+                <p class="show-down text-xl text-xl text-slate-400 text-left font-bold position: left  ml-4"> Arrivée </p>
+                <selector ref="endSelector" :oncompletion="searchCities" :onclick="onendselected" x="1" y="4"></selector>
+                <input-text name="endpoint" class="w-45 mx-auto max-w-fit" placeholder="Paris" dark="true"></input-text>
+            </div>
+        </div>
+
+
+        <p class="show-down text-xl text-xl text-slate-400 text-left font-bold position: left  ml-4"> Groupe de trajet </p>
+        <button-block > Créer le trajet </button-block>
+        <button-block > Annuler </button-block>
+    </div>
+</template>
+
+
+<script>
+import ButtonBlock from '../components/inputs/ButtonBlock.vue';
+import ButtonText from '../components/inputs/ButtonText.vue';
+import InputText from '../components/inputs/InputText.vue';
+import Topbar from "../components/topbar/Topbar.vue";
+import { goBack } from '../scripts/redirects';
+import User from "../scripts/User.js";
+
+export default {
+    name: 'Home',
+    components: {
+    Topbar,
+    ButtonBlock,
+    InputText,
+    ButtonText
+},
+    data() {
+        return { User, goBack ,
+            compteur: 0 }
+    },
+    searchCities(selector, search) {
+        BAN.searchCities(search).then(cities => {
+            let index = 0;
+            startCities = cities.map(city => ({ id: index++, value: city.city }));
+            selector.setData(startCities);
+        }).catch(err => {
+            startCities = [];
+            selector.setData(startCities);
+        });
+    },
+    reverseInputs() {
+            const start = this.startInput.value;
+            const end = this.endInput.value;
+            this.startInput.value = end;
+            this.endInput.value = start;
+        },
+    methods: {
+    incrementCounter() {
+      this.compteur++
+    }
+  }
+}
+
+</script>
