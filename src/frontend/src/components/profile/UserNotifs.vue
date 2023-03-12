692743eeadbf33eb062ff6703079ec6dc0cf4211
Antoine CONTOUX <contoux1u@etu.univ-lorraine.fr>
2023-03-12 13:21:15 +0100

    one card per notif

--- src/frontend/src/components/profile/UserNotifs.vue
+++ src/frontend/src/components/profile/UserNotifs.vue
@@ -3,7 +3,7 @@
         <p class="text-2xl text-teal-500 py-2 font-bold mx-auto"> {{ lang.MY_NOTIFS }} </p>
         <div class="flex flex-col grow justify-evenly">
             <div class="flex flex-col w-fit md:mx-auto mx-4">
-                <card class="flex grow h-fit min-w-[60vw] md:max-w-[70vw]">
+                <div class="flex grow h-fit min-w-[60vw] md:max-w-[70vw]">
                     <div v-show="loading" class="flex flex-col justify-center mx-auto">
 
                         <div class="flex flex-col justify-center py-4 my-4 rounded-lg bg-slate-100 px-4">
@@ -30,17 +30,24 @@
                     </div>
                     <div v-if="notifs.length > 0 && !loading" class="flex-col w-full">
 
-                        <button v-for="notif in notifs" :key="notif.id"
-                                class="flex py-4 my-4 rounded-lg bg-slate-100 px-4 border-2 border-transparent
-                                    hover:border-slate-200 transition-all w-full text-left"
-                        >
-                            <p class="basis-1/3">{{notif.title}}</p>
-                            <p class="basis-2/3"> {{ notif.message }} </p>
+                        <card v-for="notif in notifs" :key="notif.id"
+                                class="py-4 my-4 rounded-lg px-4 hover:bg-slate-100 transition-all w-full text-left
+                                        text-slate-600">
+                            <div class="w-full">
+                                <p class="font-bold">
+                                    <span class="text-slate-400 mr-4">{{ new Date(notif.createdAt).toLocaleString() }}</span>
+                                    <span class="">{{ notif.title }}</span>
+                                </p>
+                                <p class=""> {{ notif.message }} </p>
+                            </div>
+                            <div class="">
+                                <button-block class="" color="red" @click="deleteOne(notif.id)">Supprimer</button-block>
+                            </div>
 
-                        </button>
+                        </card>
 
                     </div>
-                </card>
+                </div>
             </div>
         </div>
     </div>
@@ -51,10 +58,13 @@ import Lang from '../../scripts/Lang'
 import API from "../../scripts/API";
 import User from "../../scripts/User";
 import Card from "../cards/Card.vue";
+import ButtonBlock from "../inputs/ButtonBlock.vue";
+import ButtonText from "../inputs/ButtonText.vue";
+import ButtonTab from "../inputs/ButtonTab.vue";
 
 export default {
     name: "UserNotifs",
-    components: {Card},
+    components: {ButtonTab, ButtonText, ButtonBlock, Card},
     data() {
         return {
             lang: Lang.CurrentLang,
@@ -72,6 +82,7 @@ export default {
             this.loading = true;
             API.execute_logged(API.ROUTE.MY_NOTIFS, API.METHOD.GET, User.CurrentUser?.getCredentials()).then((data) => {
                 console.log(data);
+                data.sort((a, b) => new Date(b.createdAt) - new Date(a.createdAt));
                 this.notifs = data;
             }).catch(err => {
                 console.error(err);
@@ -80,7 +91,11 @@ export default {
             }).finally(() => {
                 this.loading = false;
             });
-        }
+        },
+        deleteOne(id) {
+            this.notifs = this.notifs.filter(notif => notif.id !== id);
+            // TODO: delete notif
+        },
     }
 }
 </script>
