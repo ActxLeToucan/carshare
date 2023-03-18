4ccb5ed661cdefde5ba667b5043ff171a8b4de5f
kenza <kenza.bouhadoun9@univ-lorraine.fr>
2023-03-18 17:09:57 +0100

    regler les beug d'affichage

--- src/frontend/src/components/profile/UserTrips.vue
+++ src/frontend/src/components/profile/UserTrips.vue
@@ -3,31 +3,15 @@
         <p class="text-2xl text-teal-500 py-2 font-bold mx-auto"> {{ lang.MY_TRIPS }} </p>
         <p class="ml-5 text-2xl text-slate-400"  style="margin-top: 3em">{{ lang.FUTURE_TRIPS }}</p>
         
-        <div class="flex flex-col justify-center py-4 my-4 rounded-lg bg-slate-100 px-4" style="margin-left: 1em; margin-right: 40em; position: relative;">
-            <p class="text-2xl text-slate-500 py-2 font-semibold">{{ lang.TRIP_OFF }}</p>
-            <div class="flex justify-between">
-            <p class="text-xl text-slate-500 py-2 font-semibold">villeD</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold">heureD</p>
+        <div class="flex w-full flex-col px-8 space-y-4 pt-4 max-w-full min-w-0">
+               <user-trips-card
+                    class="min-w-0 w-full show-up" v-for="group in groups"
+                    :data="group" :key="group?.id" :onclick="onCardClicked">
+                </user-trips-card>
             </div>
-            <div class="flex justify-between">
-            <p class="text-xl text-slate-500 py-2 font-semibold">villeA</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold">heureA</p>
-        </div>
-            <button-block v-on:click="actionOnClick" class="show-right flex flex-col items-right md:w-min w-full mx-auto overflow-hidden text-xl" style="position: absolute; bottom: 0; right: 0; margin-right: 0.5em ;margin-bottom: 0.5em">{{ lang.CANCEL }}</button-block>
-            <p class="text-xl text-slate-500 py-2 font-semibold">{{ lang.PASSENGER }}</p>
-        </div>
+
         
         <p class="ml-5 text-2xl text-slate-400" style="margin-top: 5em ;">{{ lang.PAST_TRIPS }}</p>
-                   
-        <div class="flex justify-between py-4 my-4 rounded-lg bg-slate-100 px-4 justify-between" style="margin-left: 1em; margin-right: 15em; position: relative;">
-            <p class="text-xl text-slate-500 py-2 font-semibold">Date</p> 
-           <!-- <p class="text-xl text-slate-500 py-2 font-semibold" name="departureDate" :label="lang.DATE" :value="formProperties.properties.departureDate" @input="formProperties.properties.departureDate = $event.target.value"></p> -->
-            <p class="text-xl text-slate-500 py-2 font-semibold">heureD</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold">villeD</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold">heureA</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold">villeA</p>
-            <p class="text-xl text-slate-500 py-2 font-semibold" style="text-align: right">{{ lang.PASSENGER }}</p>
-        </div>
 
 
  
@@ -38,6 +22,8 @@
 import Lang from '../../scripts/Lang';
 import ButtonBlock from '../inputs/ButtonBlock.vue';
 import InputText from '../inputs/InputText.vue';
+import UserTripsCard from './UserTripsCard.vue';
+
 
 import {
     XMarkIcon
@@ -46,15 +32,38 @@ import API from '../../scripts/API';
 import User from '../../scripts/User';
 import { Log } from '../../scripts/Logs';
 
+
+function search(obj) {
+    obj.searchBar.buttonEnabled = false;
+    const log = obj.searchLog(Lang.CurrentLang.SEARCHING + "...", Log.INFO);
+
+    const value = obj.$refs['query-zone'].querySelector('input').value;
+
+    API.execute_logged(API.ROUTE.GROUPS, API.METHOD.GET, User.CurrentUser?.getCredentials() ,{ search : value}).then((data) => {
+        obj.groups = data.data ?? data.group;
+        log.delete();
+        console.log("succes");
+    }).catch((err) => {
+        log.update(Lang.CurrentLang.ERROR + " : " + err.message, Log.ERROR);
+        setTimeout(() => { log.delete(); }, 4000);
+    }).finally(() => {
+        obj.searchBar.buttonEnabled = true;
+    });
+    console.log("searshing ");
+}
+
+
 export default {
     name: "UserTrips",
     components: {
         ButtonBlock,
         XMarkIcon,
         InputText,
+        userTripsCard,
     },
     data() {
         return { 
+            groups: [],
             lang: Lang.CurrentLang,
                 formProperties: {
                 properties: {
