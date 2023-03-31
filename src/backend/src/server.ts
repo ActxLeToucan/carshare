d76fb595b629b0dd487daa6d835c3f84b8b9fd5e
CONTOUX Antoine <antoine.contoux1@etu.univ-lorraine.fr>
2023-03-31 21:16:53 +0000

    Revert changes on server.ts 6faa4ff7

--- src/backend/src/server.ts
+++ src/backend/src/server.ts
@@ -3,7 +3,7 @@ import { app } from './app';
 import logger from './tools/logger';
 import { initMailer } from './tools/mailer';
 
-const port = normalizePort(process.env.PORT ?? '4000');
+const port = normalizePort(process.env.PORT ?? '3000');
 app.set('port', port);
 
 const server = http.createServer(app);
