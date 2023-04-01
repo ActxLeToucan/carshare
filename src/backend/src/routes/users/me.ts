dbfc8594562bc95a5b4cf8f59ba2d5696124e37f
kenza <kenza.bouhadoun9@univ-lorraine.fr>
2023-04-01 04:24:00 +0200

    resolution des conflits

--- src/backend/src/routes/users/me.ts
+++ src/backend/src/routes/users/me.ts
@@ -1,43 +1,11 @@
-import type express from 'express';
-import { displayableUserPrivate, error, info, sendMsg } from '../../tools/translator';
-import * as validator from '../../tools/validator';
-import bcrypt from 'bcrypt';
-import { prisma } from '../../app';
-import * as _user from './_common';
+import express from 'express';
 
-const usersController = require('../../controllers/users');
+const router = express.Router();
+const controller = require('../../controllers/users/me');
 
-exports.getMe = (req: express.Request, res: express.Response, _: express.NextFunction) => {
-    res.status(200).json(displayableUserPrivate(res.locals.user));
-}
+router.get('/', controller.getMe);
+router.delete('/', controller.deleteMe);
+router.patch('/', controller.updateMe);
+router.patch('/password', controller.updateMyPassword);
 
-exports.deleteMe = (req: express.Request, res: express.Response, _: express.NextFunction) => {
-    if (!validator.checkPasswordField(req.body.password, req, res, false)) return;
-
-    bcrypt.compare(req.body.password, res.locals.user.password)
-        .then((valid) => {
-            if (!valid) {
-                sendMsg(req, res, error.auth.wrongPassword);
-                return;
-            }
-
-            prisma.user.delete({ where: { id: res.locals.user.id } })
-                .then(() => { sendMsg(req, res, info.user.deleted); })
-                .catch((err) => {
-                    console.error(err);
-                    sendMsg(req, res, error.generic.internalError);
-                });
-        }).catch((err) => {
-            console.error(err);
-            sendMsg(req, res, error.generic.internalError);
-        });
-}
-
-exports.updateMe = (req: express.Request, res: express.Response, _: express.NextFunction) => {
-    _user.update(req, res, res.locals.user.id, false).catch((err) => {
-        console.error(err);
-        sendMsg(req, res, error.generic.internalError);
-    });
-}
-
-exports.updateMyPassword = usersController.updatePassword;
+module.exports = router;
