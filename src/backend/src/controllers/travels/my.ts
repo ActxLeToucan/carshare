1ee5304758a3e13a26f1df5073dcbe3006a91bcb
kenza <kenza.bouhadoun9@univ-lorraine.fr>
2023-04-01 04:05:22 +0200

    resolution des conflits

--- src/backend/src/controllers/travels/my.ts
+++ src/backend/src/controllers/travels/my.ts
@@ -1,21 +1,5 @@
 import type express from 'express';
 import { preparePagination } from '../_common';
-<<<<<<< HEAD
-import { prisma } from '../../app';
-import { error, sendMsg } from '../../tools/translator';
-
-exports.getMyTravelsAsDriver = (req: express.Request, res: express.Response, _: express.NextFunction) => {
-    const pagination = preparePagination(req, false);
-
-    prisma.travel.count({
-        where: { driverId: res.locals.user.id }
-    }).then((count) => {
-        prisma.travel.findMany({
-            where: { driverId: res.locals.user.id },
-            ...pagination.pagination
-        }).then(travels => {
-            res.status(200).json(pagination.results(travels, count));
-=======
 import * as validator from '../../tools/validator';
 import { prisma } from '../../app';
 import { displayableSteps, error, info, notifs, notify, sendMsg } from '../../tools/translator';
@@ -116,34 +100,10 @@ exports.getMyTravels = (req: express.Request, res: express.Response, _: express.
                 console.error(err);
                 sendMsg(req, res, error.generic.internalError);
             });
->>>>>>> d76fb595b629b0dd487daa6d835c3f84b8b9fd5e
         }).catch((err) => {
             console.error(err);
             sendMsg(req, res, error.generic.internalError);
         });
-<<<<<<< HEAD
-    }).catch((err) => {
-        console.error(err);
-        sendMsg(req, res, error.generic.internalError);
-    });
-}
-
-exports.getMyTravelsAsPassenger = (req: express.Request, res: express.Response, _: express.NextFunction) => {
-    const pagination = preparePagination(req, false);
-
-    prisma.passenger.count({
-        where: { passengerId: res.locals.user.id }
-    }).then((count) => {
-        prisma.passenger.findMany({
-            where: { passengerId: res.locals.user.id },
-            include: {
-                departure: true,
-                arrival: true
-            },
-            ...pagination.pagination
-        }).then(passengers => {
-            res.status(200).json(pagination.results(passengers, count));
-=======
 }
 
 exports.cancelMyTravel = (req: express.Request, res: express.Response, _: express.NextFunction) => {
@@ -220,7 +180,6 @@ exports.cancelMyTravel = (req: express.Request, res: express.Response, _: expres
                 console.error(err);
                 sendMsg(req, res, error.generic.internalError);
             });
->>>>>>> d76fb595b629b0dd487daa6d835c3f84b8b9fd5e
         }).catch((err) => {
             console.error(err);
             sendMsg(req, res, error.generic.internalError);
@@ -230,8 +189,6 @@ exports.cancelMyTravel = (req: express.Request, res: express.Response, _: expres
         sendMsg(req, res, error.generic.internalError);
     });
 }
-<<<<<<< HEAD
-=======
 
 exports.updateMyTravel = (req: express.Request, res: express.Response, _: express.NextFunction) => {
     _travel.update(req, res, false).catch((err) => {
@@ -239,4 +196,3 @@ exports.updateMyTravel = (req: express.Request, res: express.Response, _: expres
         sendMsg(req, res, error.generic.internalError);
     });
 }
->>>>>>> d76fb595b629b0dd487daa6d835c3f84b8b9fd5e
