5a8abd9db2c3ecdb860b5f2ae3dc60c9347acbe6
FurWaz <fur.waz06@gmail.com>
2023-04-04 13:24:09 +0200

    Changed input text color

--- src/frontend/src/components/inputs/InputChoice.vue
+++ src/frontend/src/components/inputs/InputChoice.vue
@@ -11,7 +11,7 @@
                 :disabled="disabled"
                 class="flex w-fit h-fit bg-white dark:bg-slate-600 rounded-md font-bold text-lg whitespace-nowrap text-ellipsis outline-none
                        min-w-0 max-w-full outline-transparent px-4 py-2 border-b-4 border-slate-200 dark:border-slate-700 transition-all focus:border-teal-500"
-                :class="disabled? ' bg-slate-100 dark:bg-slate-700 text-slate-300 dark:text-slate-300 ' : ' bg-white dark:bg-slate-600 text-slate-400 dark:text-slate-200 hover:border-slate-300 hover:dark:border-slate-500 '"
+                :class="disabled? ' bg-slate-100 dark:bg-slate-700 text-slate-400 dark:text-slate-300 ' : ' bg-white dark:bg-slate-600 text-slate-500 dark:text-slate-200 hover:border-slate-300 hover:dark:border-slate-500 '"
             >
                 <option
                     v-for="el in elements"
